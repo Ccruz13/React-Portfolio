acc02b966873b069d10ac3a8a0d4b6ca238e2e8e
Ccruz13 <88520938+Ccruz13@users.noreply.github.com>
2022-01-28 00:20:12 -0500

    updated hero

--- portfolio/src/components/Hero/index.js
+++ portfolio/src/components/Hero/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './hero.css';
+import video from '../../../public/videos/video.mp4';
 
 function Hero (){
     return(
         <div className="hero-container">
-            <video src='/videos/video.mp4' autoPlay loop muted />
+            <video src={video} autoPlay loop muted />
             <h2>Charlie's Portfolio</h2>
             <p>Full Stack Developer</p>
         </div>
