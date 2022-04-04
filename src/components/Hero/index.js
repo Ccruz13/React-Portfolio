00d947baa6e3554a5f6339b5a929dc9eb90af6d4
Ccruz13 <88520938+Ccruz13@users.noreply.github.com>
2022-04-04 14:22:26 -0400

    updated hero

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import './hero.css';
 import video from '../../videos/video.mp4'
+import Typical from 'react-typical'
 
 function Hero (){
     return(
         <div className="hero-container">
             <video src={video} autoPlay loop muted />
-            <h2>Charlie's Portfolio</h2>
-            <p>Full Stack Developer</p>
+            <h2>Hi, my name is Charlie</h2>
+            <p>
+                <Typical 
+                loop={Infinity}
+                wrapper='b'
+                steps={[
+                    'I am a Full-Stack Developer', 2000,
+                    'I am Creative', 2000,
+                    'I love to Develop', 2000
+                ]}
+                />
+            </p>
+            <h4>Welcome to my site!</h4>
         </div>
     )
 }
